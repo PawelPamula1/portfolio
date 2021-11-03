0cb36a65ae1ba34270ba783d88f2d2b4eef03789
PawelPamula1 <pawelpamula003@gmail.com>
2021-11-03 20:56:14 +0100

    adding responsive navbar

--- src/components/Navigation/MainHeader.js
+++ src/components/Navigation/MainHeader.js
@@ -1,29 +1,70 @@
-import React from 'react';
+import {
+  faBars,
+  faCross,
+  faHamburger,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './MainHeader.module.css';
 import Socials from './Socials';
 
 const MainHeader = () => {
-  return (
-    <nav className={classes.nav}>
-      <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
+  const [isClicked, setIsClicked] = useState(false);
 
-        <li>
-          <NavLink activeClassName={classes.active} to="/projects">
-            Projects
-          </NavLink>
-        </li>
-        <li>
-          <NavLink activeClassName={classes.active} to="/contact">
-            Contact
-          </NavLink>
-        </li>
-      </ul>
-      <Socials />
-    </nav>
+  const clickHandler = () => {
+    setIsClicked(!isClicked);
+  };
+  return (
+    <>
+      <nav
+        className={
+          isClicked ? `${classes.nav} ${classes.clicked}` : `${classes.nav}`
+        }
+      >
+        <ul>
+          <li>
+            <NavLink to="/" onClick={clickHandler}>
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              activeClassName={classes.active}
+              to="/projects"
+              onClick={clickHandler}
+            >
+              Projects
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              activeClassName={classes.active}
+              to="/contact"
+              onClick={clickHandler}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+        <Socials />
+      </nav>
+      {isClicked ? (
+        <FontAwesomeIcon
+          icon={faTimes}
+          className={classes.cross}
+          onClick={clickHandler}
+        />
+      ) : (
+        <FontAwesomeIcon
+          icon={faBars}
+          className={classes.hamburger}
+          onClick={clickHandler}
+        />
+      )}
+    </>
   );
 };
 
